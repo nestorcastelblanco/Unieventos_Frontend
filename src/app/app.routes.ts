d79fe3cf331a063cc0c160dc0277275b99f7fd58
nestorcastelblanco <nestorfelipe10@gmail.com>
2024-10-24 10:54:56 -0500

    Se termino la guia

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { InicioComponent } from './componentes/inicio/inicio.component';
 import { LoginComponent } from './componentes/login/login.component';
 import { RegistroComponent } from './componentes/registro/registro.component';
 import { CrearEventoComponent } from './componentes/crear-evento/crear-evento.component';
+import { GestionEventosComponent } from './componentes/gestion-eventos/gestion-eventos.component';
 
 
 export const routes: Routes = [
@@ -10,5 +11,6 @@ export const routes: Routes = [
    { path: 'login', component: LoginComponent },
    { path: 'registro', component: RegistroComponent },
    { path: 'crear-evento', component: CrearEventoComponent },
+   { path: "gestion-eventos", component: GestionEventosComponent },
    { path: "**", pathMatch: "full", redirectTo: "" }
 ];
---
+++ src/app/componentes/gestion-eventos/gestion-eventos.component.ts
@@ -0,0 +1,82 @@
+import { Component } from '@angular/core';
+import { EventosService } from '../../servicios/eventos.service';
+import { RouterModule } from '@angular/router';
+import { EventoDTO } from '../../dto/evento-dto';
+import Swal from 'sweetalert2';
+
+@Component({
+ selector: 'app-gestion-eventos',
+ standalone: true,
+ imports: [RouterModule],
+ templateUrl: './gestion-eventos.component.html',
+ styleUrl: './gestion-eventos.component.css'
+})
+export class GestionEventosComponent {
+
+  textoBtnEliminar:string = "";
+  seleccionados: EventoDTO[];
+  eventos: EventoDTO[];
+
+ constructor(public eventosService:EventosService) {
+  this.seleccionados = [];
+   this.eventos = eventosService.listar();
+ }
+
+ public seleccionar(evento: EventoDTO, estado: boolean) {
+
+  if (estado) {
+    this.seleccionados.push(evento);
+  } else {
+    this.seleccionados.splice(this.seleccionados.indexOf(evento), 1);
+  }
+ 
+ 
+  this.actualizarMensaje();
+ 
+ 
+ }
+ 
+ 
+ private actualizarMensaje() {
+  const tam = this.seleccionados.length;
+ 
+ 
+  if (tam != 0) {
+    if (tam == 1) {
+      this.textoBtnEliminar = "1 elemento";
+    } else {
+      this.textoBtnEliminar = tam + " elementos";
+    }
+  } else {
+    this.textoBtnEliminar = "";
+  }
+ }
+
+ public confirmarEliminacion() {
+  Swal.fire({
+    title: "Estás seguro?",
+    text: "Esta acción cambiará el estado de los eventos a Inactivos.",
+    icon: "error",
+    showCancelButton: true,
+    confirmButtonText: "Confirmar",
+    cancelButtonText: "Cancelar",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      this.eliminarEventos();
+      Swal.fire("Eliminados!", "Los eventos seleccionados han sido eliminados.", "success");
+    }
+  });
+ }
+
+ public eliminarEventos() {
+  this.seleccionados.forEach(e1 => {
+    this.eventosService.eliminar(e1.id);
+    this.eventos = this.eventos.filter(e2 => e2.id !== e1.id);
+  });
+  this.seleccionados = [];
+  this.actualizarMensaje();
+ 
+ 
+ }
+ 
+}
