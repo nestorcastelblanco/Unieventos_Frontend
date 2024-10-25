f6d6b8dd786de00a5c62762c4211f251aa8a5220
JulianLadino04 <andresjul043011@gmail.com>
2024-10-25 12:34:38 -0500

    Ventana Cambiar Contraseña

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { RegistroComponent } from './componentes/registro/registro.component';
 import { CrearEventoComponent } from './componentes/crear-evento/crear-evento.component';
 import { GestionEventosComponent } from './componentes/gestion-eventos/gestion-eventos.component';
 import { HistorialEventosComponent } from './componentes/historial-eventos/historial-eventos.component';
+import { CambiarPasswordComponent } from './componentes/cambiar-password/cambiar-password.component';
 
 
 
@@ -15,5 +16,6 @@ export const routes: Routes = [
    { path: 'registro', component: RegistroComponent },
    { path: "historial-eventos", component: HistorialEventosComponent },
    { path: 'gestion-eventos', component: GestionEventosComponent },
+   { path: 'cambiar-password', component: CambiarPasswordComponent },
    { path: "**", pathMatch: "full", redirectTo: "" }
 ];
---
+++ src/app/componentes/cambiar-password/cambiar-password.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, AbstractControlOptions } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-cambiar-password',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './cambiar-password.component.html',
+  styleUrls: ['./cambiar-password.component.css']
+})
+export class CambiarPasswordComponent {
+
+  registroForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private router: Router) { 
+    this.crearFormulario();
+  }
+
+  private crearFormulario() {
+    this.registroForm = this.formBuilder.group({
+      password: ['', [Validators.required, Validators.maxLength(10), Validators.minLength(7)]],
+      confirmaPassword: ['', [Validators.required]]
+    }, { validators: this.passwordsMatchValidator } as AbstractControlOptions);
+  }
+
+  passwordsMatchValidator(formGroup: FormGroup) {
+    const password = formGroup.get('password')?.value;
+    const confirmaPassword = formGroup.get('confirmaPassword')?.value;
+
+    return password === confirmaPassword ? null : { passwordsMismatch: true };
+  }
+
+  public cambiarContrasenia() {
+    if (this.registroForm.valid) {
+      console.log(this.registroForm.value);
+      this.router.navigate(['/login']); // Redirige a la página de login
+    }
+  }
+}
