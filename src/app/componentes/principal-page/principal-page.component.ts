70c3447a9a03c6832faa7cafbca96825c05a45f4
Hulbert Alejandro <hulberta.arangof@uqvirtual.edu.co>
2024-11-14 22:04:44 -0500

    Firebase

--- src/app/componentes/principal-page/principal-page.component.ts
+++ src/app/componentes/principal-page/principal-page.component.ts
@@ -6,16 +6,19 @@ import { FooterComponent } from "../../././componentes/footer/footer.component";
 import { CarruselImagenesComponent } from "../../././componentes/carrusel-imagenes/carrusel-imagenes.component";
 import { PublicoService } from '../../servicios/publico.service';
 import { EventoDTO } from '../../dto/EventoDTOs/evento-dto';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-principal-page',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule],
   templateUrl: './principal-page.component.html',
   styleUrl: './principal-page.component.css'
 })
 export class PrincipalPageComponent {
   public eventos: EventoDTO[]; // Declaración de la variable `eventos`
+  public eventosFiltrados: EventoDTO[] = []; // Lista de eventos filtrados
+  public filtros = { nombre: '', tipo: '', ciudad: '' }; // Filtros de búsqueda
 
   constructor(private publicoService: PublicoService) {
     this.eventos = [];
@@ -31,6 +34,18 @@ export class PrincipalPageComponent {
       error: (error) => {
         console.error(error);
       },
+    });
+  }
+
+  // Filtra los eventos según los valores de los filtros
+  public filtrarEventos() {
+    this.eventosFiltrados = this.eventos.filter(evento => {
+      const nombreMatch = evento.nombre.toLowerCase().includes(this.filtros.nombre.toLowerCase());
+      const tipoMatch = evento.tipo.toLowerCase().includes(this.filtros.tipo.toLowerCase());
+      const ciudadMatch = evento.direccion.toLowerCase().includes(this.filtros.ciudad.toLowerCase());
+
+      // Devuelve `true` si cumple con todos los filtros, `false` si no
+      return nombreMatch && tipoMatch && ciudadMatch;
     });
   }
 }
